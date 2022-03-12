ff81cfb79aa1db2be8b1c1470fa30f91a809d45b
Cam Moore <cammymoore07@gmail.com>
2022-03-11 15:35:14 -1000

    Added Bootstrap theming.

--- app/imports/startup/client/Startup.jsx
+++ app/imports/startup/client/Startup.jsx
@@ -5,6 +5,9 @@ import App from '../../ui/layouts/App.jsx';
 
 /* global document */
 
+// Importing the Bootstrap CSS
+import './css/theme.css';
+
 // Startup the application by rendering the App layout component.
 Meteor.startup(() => {
   render(<App />, document.getElementById('root'));
