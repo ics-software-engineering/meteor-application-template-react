2e6ddce87d524e805d53c4beffc457aea81b9522
Cam Moore <cammymoore07@gmail.com>
2022-05-01 08:56:57 -1000

    Updated testcafe scripts.

--- app/tests/navbar.component.js
+++ app/tests/navbar.component.js
@@ -11,28 +11,44 @@ class NavBar {
     }
   }
 
-  async gotoSigninPage(testController) {
+  async gotoSignInPage(testController) {
     await this.ensureLogout(testController);
+    const visible = await Selector('#basic-navbar-nav').visible;
+    if (!visible) {
+      await testController.click('button.navbar-toggler');
+    }
     await testController.click('#login-dropdown');
     await testController.click('#login-dropdown-sign-in');
   }
 
   /** Check that the specified user is currently logged in. */
   async isLoggedIn(testController, username) {
+    const visible = await Selector('#basic-navbar-nav').visible;
+    if (!visible) {
+      await testController.click('button.navbar-toggler');
+    }
     const loggedInUser = Selector('#navbar-current-user').innerText;
     await testController.expect(loggedInUser).eql(username);
   }
 
   /** Check that someone is logged in, then click items to logout. */
   async logout(testController) {
+    const visible = await Selector('#basic-navbar-nav').visible;
+    if (!visible) {
+      await testController.click('button.navbar-toggler');
+    }
     await testController.expect(Selector('#navbar-current-user').exists).ok();
     await testController.click('#navbar-current-user');
     await testController.click('#navbar-sign-out');
   }
 
   /** Pull down login menu, go to sign up page. */
-  async gotoSignupPage(testController) {
+  async gotoSignUpPage(testController) {
     await this.ensureLogout(testController);
+    const visible = await Selector('#basic-navbar-nav').visible;
+    if (!visible) {
+      await testController.click('button.navbar-toggler');
+    }
     await testController.click('#login-dropdown');
     await testController.click('#login-dropdown-sign-up');
   }
--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -8,7 +8,7 @@ import { navBar } from './navbar.component';
 /** Credentials for one of the sample users defined in settings.development.json. */
 const credentials = { username: 'john@foo.com', password: 'changeme' };
 
-fixture('meteor-application-template-react localhost test with default db')
+fixture('meteor-react-bootstrap-template localhost test with default db')
   .page('http://localhost:3000');
 
 test('Test that landing page shows up', async (testController) => {
@@ -16,7 +16,7 @@ test('Test that landing page shows up', async (testController) => {
 });
 
 test('Test that signin and signout work', async (testController) => {
-  await navBar.gotoSigninPage(testController);
+  await navBar.gotoSignInPage(testController);
   await signinPage.signin(testController, credentials.username, credentials.password);
   await navBar.isLoggedIn(testController, credentials.username);
   await navBar.logout(testController);
