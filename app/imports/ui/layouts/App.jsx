6754e6441a4627dda2b86e0f5ed31c7b485d77c7
Cam Moore <cammymoore07@gmail.com>
2022-05-25 16:20:49 -1000

    Checkpoint

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Navigate, Redirect } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Landing from '../pages/Landing';
 import ListStuff from '../pages/ListStuff';
@@ -14,6 +14,7 @@ import SignUp from '../pages/SignUp';
 import SignOut from '../pages/SignOut';
 import NavBar from '../components/NavBar';
 import SignIn from '../pages/SignIn';
+import NotAuthorized from '../pages/NotAuthorized';
 
 /** Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
 const App = () => (
@@ -21,69 +22,55 @@ const App = () => (
     <div>
       <NavBar/>
       <Switch>
-        <Route exact path="/" component={Landing}/>
-        <Route path="/signin" component={SignIn}/>
-        <Route path="/signup" component={SignUp}/>
-        <Route path="/signout" component={SignOut}/>
-        <ProtectedRoute path="/home" component={Landing}/>
-        <ProtectedRoute path="/list" component={ListStuff}/>
-        <ProtectedRoute path="/add" component={AddStuff}/>
-        <ProtectedRoute path="/edit/:_id" component={EditStuff}/>
-        <AdminProtectedRoute path="/admin" component={ListStuffAdmin}/>
-        <Route component={NotFound}/>
+        <Route exact path="/"><Landing/></Route>
+        <Route path="/signin"><SignIn/></Route>
+        <Route path="/signup"><SignUp/></Route>
+        <Route path="/signout"><SignOut/></Route>
+        <Route path="/home" render={() => <ProtectedRoute><Landing/></ProtectedRoute>}/>
+        <Route path="/list" render={() => <ProtectedRoute><ListStuff/></ProtectedRoute>}/>
+        <Route path="/add" render={() => <ProtectedRoute><AddStuff/></ProtectedRoute>}/>
+        <Route path="/edit/:_id" render={() => <ProtectedRoute><EditStuff/></ProtectedRoute>}/>
+        <Route path="/admin" render={() => <AdminProtectedRoute><ListStuffAdmin/></AdminProtectedRoute>}/>
+        <Route path="/notauthorized"><NotAuthorized/></Route>
+        <Route><NotFound/></Route>
       </Switch>
       <Footer/>
     </div>
   </Router>
 );
 
-/**
+/*
  * ProtectedRoute (see React Router v4 sample)
  * Checks for Meteor login before routing to the requested page, otherwise goes to signin page.
  * @param {any} { component: Component, ...rest }
  */
-const ProtectedRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={(props) => {
-      const isLogged = Meteor.userId() !== null;
-      return isLogged ?
-        (<Component {...props} />) :
-        (<Redirect to={{ pathname: '/signin', state: { from: props.location } }}/>
-        );
-    }}
-  />
-);
+const ProtectedRoute = ({ children }) => {
+  const isLogged = Meteor.userId() !== null;
+  return isLogged ? children : <Redirect to='/signin'/>;
+};
 
 /**
  * AdminProtectedRoute (see React Router v4 sample)
  * Checks for Meteor login and admin role before routing to the requested page, otherwise goes to signin page.
  * @param {any} { component: Component, ...rest }
  */
-const AdminProtectedRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={(props) => {
-      const isLogged = Meteor.userId() !== null;
-      const isAdmin = Roles.userIsInRole(Meteor.userId(), 'admin');
-      return (isLogged && isAdmin) ?
-        (<Component {...props} />) :
-        (<Redirect to={{ pathname: '/signin', state: { from: props.location } }}/>
-        );
-    }}
-  />
-);
+const AdminProtectedRoute = ({ children }) => {
+  const isLogged = Meteor.userId() !== null;
+  if (!isLogged) {
+    return <Redirect to='/signin'/>;
+  }
+  const isAdmin = Roles.userIsInRole(Meteor.userId(), 'admin');
+  return (isLogged && isAdmin) ? children : <Redirect to='/notauthorized'/>;
+};
 
 // Require a component and location to be passed to each ProtectedRoute.
 ProtectedRoute.propTypes = {
-  component: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
-  location: PropTypes.object,
+  children: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
 };
 
 // Require a component and location to be passed to each AdminProtectedRoute.
 AdminProtectedRoute.propTypes = {
-  component: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
-  location: PropTypes.object,
+  children: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
 };
 
 export default App;
---
+++ app/imports/ui/pages/NotAuthorized.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+
+/** Render a Not Found page if the user enters a URL that doesn't match any route. */
+const NotAuthorized = () => (
+  <Container>
+    <Row className="justify-content-center">
+      <Col xs={4} className="text-center">
+        <h2>
+          <p>Not Authorized</p>
+        </h2>
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default NotAuthorized;
