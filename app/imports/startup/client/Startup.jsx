cc7125c9d8c969c9b5761324c6dd6d1f037d51b4
Cam Moore <cammymoore07@gmail.com>
2022-05-26 09:49:20 -1000

    Update Startup.jsx
    Uses the v6 way to render the app.

--- app/imports/startup/client/Startup.jsx
+++ app/imports/startup/client/Startup.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Meteor } from 'meteor/meteor';
 import App from '../../ui/layouts/App.jsx';
 
@@ -10,5 +10,8 @@ import './css/theme.css';
 
 // Startup the application by rendering the App layout component.
 Meteor.startup(() => {
-  render(<App />, document.getElementById('root'));
+  const root = ReactDOM.createRoot(
+    document.getElementById('root'),
+  );
+  root.render(<App />);
 });
