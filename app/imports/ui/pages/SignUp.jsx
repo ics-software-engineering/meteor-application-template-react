23a4271a490bb59d791d3b81f41974c067662dcd
Cam Moore <cammymoore07@gmail.com>
2022-05-26 07:03:22 -1000

    Update SignUp.jsx

--- app/imports/ui/pages/SignUp.jsx
+++ app/imports/ui/pages/SignUp.jsx
@@ -34,7 +34,7 @@ const SignUp = ({ location }) => {
   };
 
   /* Display the signup form. Redirect to add page after successful registration and login. */
-  const { from } = location.state || { from: { pathname: '/add' } };
+  const { from } = location?.state || { from: { pathname: '/add' } };
   // if correct authentication, redirect to from: page instead of signup screen
   if (redirectToReferer) {
     return <Redirect to={from}/>;
