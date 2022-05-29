0f6cf1be8b112b9e44e783765caa1fd8b1a18669
Cam Moore <cammymoore07@gmail.com>
2022-05-28 16:51:51 -1000

    Slowly getting how the CSS variables work.

--- app/client/main.js
+++ app/client/main.js
@@ -1,3 +1,6 @@
+// Start the app.
 import '../imports/startup/client/Startup';
+// Import the Bootstrap css.
 import 'bootstrap/dist/css/bootstrap.min.css';
+// Override the default bootstrap styles.
 import './style.css';
