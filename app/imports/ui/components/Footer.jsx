800a4b92db5495ff168e78277ad5970a11e750e8
Cam Moore <cammymoore07@gmail.com>
2022-06-23 11:38:02 -1000

    Back to standard bootstrap light

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Col, Container } from 'react-bootstrap';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => (
-  <footer className="mt-auto py-3 bg-light text-bg-light">
+  <footer className="mt-auto py-3 bg-light">
     <Container>
       <Col className="text-center">
         Department of Information and Computer Sciences
