d064e811405eb9bf83359daa71da806ca42f16e4
Cam Moore <cammymoore07@gmail.com>
2022-06-24 11:51:10 -1000

    Using arrow functions and forEach.

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -4,15 +4,15 @@ import { Stuffs } from '../../api/stuff/Stuff.js';
 /* eslint-disable no-console */
 
 // Initialize the database with a default data document.
-function addData(data) {
+const addData = (data) => {
   console.log(`  Adding: ${data.name} (${data.owner})`);
   Stuffs.collection.insert(data);
-}
+};
 
 // Initialize the StuffsCollection if empty.
 if (Stuffs.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
-    Meteor.settings.defaultData.map(data => addData(data));
+    Meteor.settings.defaultData.forEach(data => addData(data));
   }
 }
