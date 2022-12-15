3e1e20686201c79bd2c5cb691dd8928a872a6aa6
Cam Moore <cammymoore07@gmail.com>
2022-12-15 09:18:28 -1000

    Updated Meteor and NPM packages.
    Passing test-all

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -8,7 +8,7 @@ import { navBar } from './navbar.component';
 /** Credentials for one of the sample users defined in settings.development.json. */
 const credentials = { username: 'john@foo.com', password: 'changeme' };
 
-fixture('meteor-react-bootstrap-template localhost test with default db')
+fixture('meteor-application-template-react localhost test with default db')
   .page('http://localhost:3000');
 
 test('Test that landing page shows up', async (testController) => {
