0256b467e9c5b040752bc154cdb4ae2475ce204c
Cam Moore <cammymoore07@gmail.com>
2023-05-01 12:48:19 -1000

    Changed the way we run test the landing page.

--- app/tests/landing.page.js
+++ app/tests/landing.page.js
@@ -9,7 +9,7 @@ class LandingPage {
   /** Asserts that this page is currently displayed. */
   async isDisplayed(testController) {
     // This is first test to be run. Wait 10 seconds to avoid timeouts with GitHub Actions.
-    await testController.wait(10000).expect(this.pageSelector.exists).ok();
+    await testController.expect(this.pageSelector.exists).ok();
   }
 }
 
