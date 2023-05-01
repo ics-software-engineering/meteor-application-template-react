9f852f35bb899a88e762f49a8d721d3b9447dd72
Cam Moore <cammymoore07@gmail.com>
2023-05-01 12:54:58 -1000

    Seeing if the changes to testcafe work.

--- app/tests/landing.page.js
+++ app/tests/landing.page.js
@@ -8,7 +8,6 @@ class LandingPage {
 
   /** Asserts that this page is currently displayed. */
   async isDisplayed(testController) {
-    // This is first test to be run. Wait 10 seconds to avoid timeouts with GitHub Actions.
     await testController.expect(this.pageSelector.exists).ok();
   }
 }
